303dbf2d001c54f2e1633fbb14be530d93bcf5aa
Denzov <131899365+Denzov@users.noreply.github.com>
2023-12-01 20:37:18 +0300

    Add files via upload

---
+++ Gyro/Gyro.ino
@@ -0,0 +1,84 @@
+#include <DFRobot_BMI160.h>
+#define AcX
+#define AcY 
+
+struct vec2 {
+  float x, y;
+};
+
+class Gyro {
+  private:
+    DFRobot_BMI160 bmi160;
+    const int8_t i2c_addr = 0x68;
+    int16_t accelGyro[6] = {0};
+    float result;
+    float dt;
+    float angle = 0;
+    float current_millis = 0, last_millis;
+    vec2 S;
+
+  public:
+    void init() {
+      if (bmi160.softReset() != BMI160_OK) {
+        Serial.println("reset false");
+        while (1);
+      }
+
+      //set and init the bmi160 i2c address
+      if (bmi160.I2cInit(i2c_addr) != BMI160_OK) {
+        Serial.println("init false");
+        while (1);
+      }
+    }
+    void calc() {
+      if (!bmi160.getAccelGyroData(accelGyro)) {
+        current_millis = millis();
+        dt = (current_millis - last_millis)*0.001;
+#ifdef ANG
+        result = accelGyro[2] * 0.061 * dt;
+        if (abs(result) >= 0.1) {
+          angle += result;
+        }
+#endif
+#ifdef AcX
+        result = accelGyro[3]/16384.f*9.805;
+        if (abs(result) >= 0.1){
+          S.x += result * dt *dt * 0.5;
+        }
+#endif        
+#ifdef AcY
+        result = accelGyro[4]/16384.f*9.805;
+        if (abs(result) >= 0.1){
+          S.y += result * dt *dt * 0.5;
+        }
+#endif            
+        last_millis = current_millis;
+      }
+    }
+    float get_angle() {
+      return angle;
+    }
+    float get_x() {
+      return S.x;
+    }
+    float get_y() {
+      return S.y;
+    }
+
+};
+
+
+Gyro gyro;
+void setup() {
+  Serial.begin(115200);
+  gyro.init();
+}
+
+void loop() {
+  gyro.calc();
+  Serial.print("x -- \t");
+  Serial.println(gyro.get_x());
+  Serial.print("y -- \t");
+  Serial.println(gyro.get_y());
+  Serial.println();
+}
