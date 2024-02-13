9eee9d4175db7cbc6ad390d5813612fa1a9b5ac4
Denz_ox <podlesniy0209@gmail.com>
2024-02-13 20:33:58 +0300

    com p comm

--- ComPortsCommunication/comPortCommunication.cpp
+++ ComPortsCommunication/comPortCommunication.cpp
@@ -1,2 +1,30 @@
-
 #include "comPortCommunication.h"
+
+bool comPortCommunication::init(){
+
+    com.SetPortName("\\\\.\\COM3");
+    com.SetBaudRate(9600);
+	com.SetDataSize(8);
+	com.SetParity('N');
+	com.SetStopBits(1);
+	
+    if (com.Open() == 0) {
+
+        #ifdef DEBUG
+		std::cout<<"[INFO] Was open port %s.\n" << com.GetPort().c_str() << '\n';
+        #endif
+        return 1;
+	}
+    else{
+        #ifdef DEBUG
+        std::cout<<"[Error] PORT WAS NOT OPEN.\n";
+        #endif
+
+        return 0;
+    }
+}
+template<typename T>
+void comPortCommunication::push_info(){
+    
+    
+}
--- ComPortsCommunication/comPortCommunication.h
+++ ComPortsCommunication/comPortCommunication.h
@@ -1,4 +1,15 @@
+#define DEBUG
+
+#include <iostream>
+
 #include "ceSerial.h"
+
 class comPortCommunication{
+private:
+    bool successFlag;
+    ceSerial com;
+public:
+    bool init();
+    template<typename T> void push_info();
 
 };
